9189e238d3d46f65c29ebe37b7ca69754e57f767
minal.arora <minal.arora@globallogic.com>
2020-12-25 22:47:22 +0530

    sd

--- src/db/dbfile.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose")
-const connectionUrl = 'mongodb+srv://minal:123Password@cluster0.tvrlt.mongodb.net/transfly-db?retryWrites=true&w=majority' 
-
-mongoose.connect(connectionUrl,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-})
