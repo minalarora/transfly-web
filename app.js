29db76ba98eb2c69dc67d5355292e301ddbba8aa
minal.arora <minal.arora@globallogic.com>
2020-12-26 00:02:46 +0530

    last

--- app.js
+++ app.js
@@ -1,81 +1,122 @@
-const express = require("express")
-const path = require("path")
-const multer = require("multer")
+const express  = require("express")
+const helmet=  require('helmet')
+const compression = require('compression')
+const path = require('path')
+const ejs = require('ejs')
 const app = express()
+const bodyparser = require('body-parser')
+const port  = process.env.PORT || 3000
+require("./src/db/dbfile")
 
-// View Engine Setup 
-app.set("views", path.join(__dirname, "views"))
-app.set("view engine", "ejs")
+const Admin  = require('./src/routers/admin')
+const AreaManager = require('./src/routers/areamanager')
+const Booking = require('./src/routers/booking')
+const Fieldstaff = require('./src/routers/fieldstaff')
+const Finance = require('./src/routers/finance')
+const Invoice  = require('./src/routers/invoice')
+const Mine =  require('./src/routers/mine')
+const Ticket =  require('./src/routers/ticket')
+const Transporter = require('./src/routers/transporter')
+const Vehicle = require('./src/routers/vehicle')
+const VehicleOwner = require("./src/routers/vehicleowner")
+const WebAdmin = require('./src/routers/webadmin')
+const WebAreaManager  = require('./src/routers/webareamanager')
+const WebFieldStaff =  require('./src/routers/webfieldstaff')
+const WebFinance  =  require('./src/routers/webfinance')
+const WebInvoice  =  require('./src/routers/webinvoice')
+const WebLogin  =  require('./src/routers/weblogin')
+const WebMine = require('./src/routers/webmine')
+const WebResale =  require('./src/routers/webresale')
+const WebRewardReferral = require('./src/routers/webrewardreferral')
+const WebTransporter  =  require('./src/routers/webtransporter')
+const WebVehicleOwner  =  require('./src/routers/webvehicleowner')
 
-// var upload = multer({ dest: "Upload_folder_name" }) 
-// If you do not want to use diskStorage then uncomment it 
 
-var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
 
-        // Uploads is the Upload_folder_name 
-        cb(null, "uploads")
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.fieldname + "-" + Date.now() + ".jpg")
-    }
+const viewsPath = path.join(__dirname,'./src/views')
+app.set('view engine','ejs')
+app.set('views',viewsPath)
+
+/*app.use((req,res,next)=>{
+    /*  if(req.method == 'GET')
+      {
+          res.send('GET REQUEST ARE DISABLED')
+      }
+      else
+      {
+          next()
+      }*/
+  
+      /*res.status(400).send('site is currently down')
+  })*/
+
+//  const partialsPath  = path.join(__dirname,'./partials')
+//  ejs.registerPartials(partialsPath)
+
+
+const publicPath = path.join(__dirname,'./src/public')
+app.use(express.static(publicPath))
+
+
+
+
+
+ app.use(bodyparser.json()); 
+
+// for parsing application/xwww-
+app.use(bodyparser.urlencoded({ extended: true })); 
+//form-urlencoded
+
+// for parsing multipart/form-data
+app.use(express.static('public'));
+
+app.use(WebAdmin)
+app.use(WebAreaManager)
+app.use(WebFieldStaff)
+app.use(WebFinance)
+app.use(WebInvoice)
+app.use(WebLogin)
+app.use(WebMine)
+app.use(WebResale)
+app.use(WebRewardReferral)
+app.use(WebTransporter)
+app.use(WebVehicleOwner)
+
+
+app.use(express.json())
+
+app.use(Admin)
+app.use(AreaManager)
+app.use(Booking)
+app.use(Fieldstaff)
+app.use(Finance)
+app.use(Invoice)
+app.use(Mine)
+app.use(Ticket)
+app.use(Transporter)
+app.use(Vehicle)
+app.use(VehicleOwner)
+
+app.use(helmet())
+app.use(compression())
+
+
+
+
+
+/*
+const updateMobileAndCount = async(id,mobile)=>{
+const user = await User.findByIdAndUpdate()
+const count = await User.countDocuments()
+return count
+}.then(count){
+
+}
+.catch(e)
+*/
+
+
+
+app.listen(port,()=>{
+    console.log("server is up on port", port)
 })
-
-// Define the maximum size for uploading 
-// picture i.e. 1 MB. it is optional 
-const maxSize = 1 * 1000 * 1000;
-
-var upload = multer({
-    storage: storage,
-    limits: { fileSize: maxSize },
-    fileFilter: function (req, file, cb) {
-
-        // Set the filetypes, it is optional 
-        var filetypes = /jpeg|jpg|png/;
-        var mimetype = filetypes.test(file.mimetype);
-
-        var extname = filetypes.test(path.extname(
-            file.originalname).toLowerCase());
-
-        if (mimetype && extname) {
-            return cb(null, true);
-        }
-
-        cb("Error: File upload only supports the "
-            + "following filetypes - " + filetypes);
-    }
-
-    // mypic is the name of file attribute 
-}).single("mypic");
-
-app.get("/", function (req, res) {
-    res.render("Signup");
-})
-
-app.post("/uploadProfilePicture", function (req, res, next) {
-
-    // Error MiddleWare for multer file upload, so if any 
-    // error occurs, the image would not be uploaded! 
-    upload(req, res, function (err) {
-
-        if (err) {
-
-            // ERROR occured (here it can be occured due 
-            // to uploading image of size greater than 
-            // 1MB or uploading different file type) 
-            res.send(err)
-        }
-        else {
-            console.log(req.file)
-            // SUCCESS, image successfully uploaded 
-            res.send("Success, Image uploaded!")
-        }
-    })
-})
-
-// Take any port number of your choice which 
-// is not taken by any other process 
-app.listen(8080, function (error) {
-    if (error) throw error
-    console.log("Server created Successfully on PORT 8080")
-}) 
