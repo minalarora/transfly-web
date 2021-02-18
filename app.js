be395f2b99202e275de1744b9e8b8a428ff90402
minal.arora <minal.arora@globallogic.com>
2021-02-18 15:25:52 +0530

    SD

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express")
 const https = require('https')
-const fs = require('fs')
 const helmet = require('helmet')
 const compression = require('compression')
 const path = require('path')
