1a0fda39d4b3c2504a152955a151cb8c582af078
Minal Arora <minal.arora@globallogic.com>
2021-03-03 00:10:05 +0530

    SD

--- app.js
+++ app.js
@@ -109,8 +109,8 @@ app.use(express.static(publicPath))
 app.use(bodyparser.json());
 
 // for parsing application/xwww-
-app.use(bodyparser.urlencoded({ extended: true, limit: '50mb' }));
-app.use(bodyparser.json({limit: '50mb'}));
+app.use(bodyparser.urlencoded({ extended: true}));
+// app.use(bodyparser.json({limit: '50mb'}));
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
 //form-urlencoded
